a12a049870385ffa908108bd513876261a220028
Austin Cabler <98928054+austincabler13@users.noreply.github.com>
2024-10-26 10:58:39 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+bl_info = {
+    "name": "Your Add-on Name",
+    "blender": (4, 2, 3),  # Change to your supported Blender version
+    "category": "3D View",  # Change to the appropriate category
+    "author": "Austin Cabler",
+    "version": (0, 1, 3),
+    "description": "A plugin for blender that uses Mushy's API to create models",
+    "support": "COMMUNITY",
+    "url": "https://github.com/austincabler13/3D-Maker/",
+    
+}
+
+def register():
+    print("Add-on registered.")
+
+def unregister():
+    print("Add-on unregistered.")
