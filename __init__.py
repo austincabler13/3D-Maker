bf4a33684cb86acd29049e4c02adb15c3a229d44
austincabler13 <austincabler11@gmail.com>
2024-10-27 10:37:33 -0400

    Update version number in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
-# from meshy_api import MeshyAPI
-
 bl_info = {
     "name": "3D-Maker",
     "blender": (4, 2, 3),  # Change to your supported Blender version
     "category": "3D View",  # Change to the appropriate category
     "author": "Austin Cabler",
-    "version": (0, 1, 3),
+    "version": (0, 1, 4),
     "description": "A plugin for Blender that uses Meshy's API to create models",
     "support": "COMMUNITY",
     "url": "https://github.com/austincabler13/3D-Maker/"
